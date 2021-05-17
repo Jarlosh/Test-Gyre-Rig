094a9017316bbfc5c8f4afdb0537a0262148d778
Jarl <Jarlo@bk.ru>
2021-05-17 19:49:03 +0500

    Added parameter input boxes

--- Assets/Scripts/Factory/FactoryData.cs
+++ Assets/Scripts/Factory/FactoryData.cs
@@ -1,20 +1,74 @@
-﻿using UnityEngine;
+﻿using System;
+using Factory.UIStuff;
+using UnityEngine;
 
 namespace Factory
 {
     public class FactoryData : MonoBehaviour
     {
         [SerializeField] Transform moveDirection;
+        [SerializeField] private InputBox distanceInput;
+        [SerializeField] private InputBox speedInput;
 
-        public float distance;
-        public float speed;
+        [SerializeField] private float distance = 1;
+        [SerializeField] private float speed = 1; 
         
+        [SerializeField] private float minDistance = 1;
+        [SerializeField] private float minSpeed = 1;
+        
+        public float Speed
+        {
+            get => speed;
+            set
+            {
+                if (speed == value)
+                    return;
+                speed = value;
+            }
+        }
+        public float Distance
+        {
+            get => distance;
+            set
+            {
+                if (distance == value)
+                    return;
+                distance = value;
+            }
+        }
+
+        private void Start()
+        {
+            speedInput.OnValueSubmit += OnSpeedSubmit;
+            distanceInput.OnValueSubmit += OnDistanceSubmit;
+            speedInput.SetValue(speed);
+            distanceInput.SetValue(distance);
+        }
+
+        private void OnDestroy()
+        {
+            speedInput.OnValueSubmit -= OnSpeedSubmit;
+            distanceInput.OnValueSubmit -= OnDistanceSubmit;
+        }
+
+        private void OnSpeedSubmit(InputBox box, float val)
+        {
+            speed = Mathf.Max(val, minSpeed);
+            box.SetValue(speed);
+        }
+        
+        private void OnDistanceSubmit(InputBox box, float val)
+        {
+            distance = Mathf.Max(val, minDistance);
+            box.SetValue(distance);
+        }
+
         public MoveInfo MakeInfo()
         {
             return new MoveInfo()
             {
-                distance = distance,
-                speed = speed,
+                distance = Distance,
+                speed = Speed,
                 worldDirection = moveDirection.forward
             };
         }
---
+++ Assets/Scripts/Factory/UIStuff/InputBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Factory.UIStuff
+{
+    public class InputBox : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField field;
+        [SerializeField] private float lastValue = 1;
+
+        
+        public event Action<InputBox, float> OnValueSubmit; 
+        
+        public void SetValue(float value)
+        {
+            lastValue = value;
+            field.text = value.ToString();
+        }
+        
+        private void Start()
+        {
+            field.onSubmit.AddListener(OnSubmit);
+        }
+
+        private void OnDestroy()
+        {
+            field.onSubmit.RemoveListener(OnSubmit);
+        }
+
+        private void OnSubmit(string val)
+        {
+            if (float.TryParse(val, out var res))
+                OnValueSubmit?.Invoke(this, res);
+        }
+    }
+}
